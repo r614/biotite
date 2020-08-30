d90acd756e78367dedd3a8ca5c08eb79d442170a
Roshan <rpawar@fispan.com>
2020-08-30 13:53:58 -0700

    Added serialization functions for writing to file + getter and setters.

--- src/biotite/structure/io/binarycif/file.py
+++ src/biotite/structure/io/binarycif/file.py
@@ -22,7 +22,7 @@ class BinaryCIFFile(TextFile, MutableMapping):
     
     def __init__(self):
         super().__init__()
-        self._content = {}
+        self._content = []
         self._content["encoder"] = "UNKNOWN"
         self._block_map = {}
      
@@ -61,9 +61,40 @@ class BinaryCIFFile(TextFile, MutableMapping):
         bcif_file._content = bcif_file._parse(raw_content['dataBlocks'])
         
         return bcif_file
+    
+    @classmethod
+    def write(self, file):
+        """
+        Write contents into a BinaryCIF file.
+        
+        Parameters
+        ----------
+        file : file-like object or str
+            The file to be written to.
+            Alternatively, a file path can be supplied.
+        """
+
+        serializedObject = self._serialize()
+        
+        packed_bytes = msgpack.packb(
+            serializedObject, use_bin_type=True, default=_encode_numpy
+        )
+        if isinstance(file, str):
+            with open(file, "wb") as f:
+                f.write(packed_bytes)
+        else:
+            if not is_binary(file):
+                raise TypeError("A file opened in 'binary' mode is required")
+            file.write(packed_bytes)
+
+    def _serialize(self):
+        res = []
+        for block in self._content: 
+            res.append(block._serialize())
+        return res
 
     def _parse(self, raw_blocks):
-        dataBlocks = [];
+        dataBlocks = []
 
         for block in raw_blocks:
             current = DataBlock(block["header"], block["categories"])
@@ -100,11 +131,9 @@ class BinaryCIFFile(TextFile, MutableMapping):
             
         Returns
         -------
-        category_dict : dict of (str or ndarray, dtype=str) or None
-            A entry keyed dictionary. The corresponding values are
-            strings or array of strings
-            Returns None, if the data block does not contain the given
-            category.
+        Category: A Category object. 
+                Returns None, if the data block does not contain the given
+                category.
         """
         if block is None:
             block = self.get_block_headers()[0]
@@ -144,10 +173,13 @@ class BinaryCIFFile(TextFile, MutableMapping):
             block = self.get_block_headers()[0]
         
         if block: 
-            self._categories[(category, block)] = category_dict
+            self._block_map(block).set_category(category_dict)
         else:
             block = "data_" + category_dict.get("name")
-            self._categories[(category, block)] = category_dict
+            new_block = DataBlock(block)
+            new_block.set_category(category_dict)
+            self._content.append(new_block)
+            self._block_map[block] = new_block
 
     def __delitem__():
         return 
@@ -181,6 +213,24 @@ class DataBlock(MutableMapping):
             res.append(current)
             self._category_map[category['name']] = current
         return res
+    
+    def get_category(self, category_name):
+        return self._category_map.get(category_name)
+
+    def set_category(self, category_name, category_dict):
+        if self.get_category(category_name):
+            self.get_category(category_name).set_category(category_dict)
+        
+        new_category = Category(category_dict['name'],
+                                category_dict['rowCount'],
+                                category_dict['columns'])
+        self._categories.append(new_category)
+        self._category_map[category_dict['name']] = new_category
+
+    def _serialize(self):
+        res = []
+        for category in self._categories:
+            res.append(category._serialize())
 
 
 class Category(MutableMapping):
@@ -194,6 +244,17 @@ class Category(MutableMapping):
         for col in raw_columns:
             res.append(Column(col['name'], col['data'], col['mask']))
         return res
+
+    def _serialize(self):
+        res = []
+        for column in columns:
+            res.append(columns._serialize())
+        return res
+
+    def set_category(self, category_dict):
+        self._columns = self._build_columns(category_dict.get('columns'))
+        self._name = category_dict.get('name')
+        self._row_count = category_dict.get('rowCount')
 
     def __delitem__():
         return 
@@ -216,19 +277,44 @@ class Column(MutableMapping):
         self._data = Data(data['data'], data['encoding'])
         self._name = name
         self._mask = Data(mask.get('data'), mask.get('encoding'))
+    
+    def _serialize(self):
+        if not mask:
+            return {
+                'data': self._data._serialize()
+            }
+        return {
+            'data': self._data._serialize(),
+            'mask': self._mask._serialize()
+        }
 
 
 class Data(MutableMapping):
     def __init__(self, data=None, encoding=None, decoded=False):
         self._data = data
         self._encoding = encoding
-        self.decoded = decoded
+        # self.decoded = self._decode(self._data)
         self.is_decoded = decoded
+        self.decoded_data = None 
+
+    def _serialize(self):
+        if not self.is_decoded:
+            return {
+                'data': self.data
+                'encoding': self.encoding
+                }
+        return self.encode(self.data, self._encoding)
 
     def __decode(self):
         // TODO
+
+        self.decoded_data = decode_data({'data': self._data,
+                                        'encoding': self._encoding})
         return None
-        
+    
+    def _encode(self):
+        // TODO 
+        return None
 
 
 
